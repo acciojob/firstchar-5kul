74135874226c8bfcf2bc4246d9694e5f2e9b337e
KULDEEP MISHRA <158719940+5kul@users.noreply.github.com>
2025-07-25 10:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function firstChar(text) {
-  // your code here
+   const trimmed = text.trimStart();
+   return trimmed.length > 0 ? trimmed.charAt(0) : '';
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
 alert(firstChar(text));
